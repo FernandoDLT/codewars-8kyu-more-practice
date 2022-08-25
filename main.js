e1701a13c25ab47ed1a5472e543281c388527d0f
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-08-25 16:59:25 -0500

    More and more practice

--- main.js
+++ main.js
@@ -22,3 +22,23 @@ console.log(getGrades(98,100,93));
 console.log(getGrades(5,40,93));
 console.log(getGrades(70,86,93));
 console.log(getGrades(85,40,93));
+
+function getGrade(g1,g2,g3) {
+    var score = (g1 + g2 + g3) / 3;
+    if (score >= 90 && score <= 100) {
+        return 'A - You passed with flying colors!';
+    }else if (score >= 80 && score < 90) {
+        return 'B - Great job!';
+    }else if (score >= 70 && score < 80) {
+        return 'C - Needs some improvement'; 
+    }else if (score >= 60 && score < 70) {
+        return 'D - You can do much better'
+    }else {
+        return 'F - I don\'t know what to say...'
+    }
+}
+console.log(getGrade(98,100,93));
+console.log(getGrade(5,40,93));
+console.log(getGrade(65,40,93));
+console.log(getGrade(70,86,93));
+console.log(getGrade(85,40,93));
